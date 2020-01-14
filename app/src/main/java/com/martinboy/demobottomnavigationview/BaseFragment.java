9f6eed069e716dc5eabfd31a88b1087704ccdcb8
linpeichen <martin.lin@futurechaser.com.tw>
2020-01-15 01:55:50 +0800

    Init Project

---
+++ app/src/main/java/com/martinboy/demobottomnavigationview/BaseFragment.java
@@ -0,0 +1,39 @@
+package com.martinboy.demobottomnavigationview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+    public static BaseFragment newInstance(String info) {
+        Bundle args = new Bundle();
+        BaseFragment fragment = new BaseFragment();
+        args.putString("info", info);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.basefragment, null);
+        TextView tvInfo = view.findViewById(R.id.textView);
+        if(getArguments() != null) {
+            tvInfo.setText(getArguments().getString("info", ""));
+        }
+        tvInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Don't click me.please!.", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
